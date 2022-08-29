9eacbf69c079aa81aa284cb4ce98d818004b7f1d
dh00mk3tu <cyberhybird@hotmail.com>
2022-08-29 12:14:28 +0530

    Fundamentals

---
+++ Module-1/Fundamentals/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.print("Hello World");
+        System.out.println("This is another line");
+        System.out.print("line after println");
+    }
+}
