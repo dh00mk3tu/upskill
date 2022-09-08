650077b9fb369ed8444ec33124cd0bc745f639dd
dh00mk3tu <cyberhybird@hotmail.com>
2022-09-08 10:22:33 +0530

    L5 & L6

--- Module-1/Fundamentals/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String args[]) {
-        System.out.print("Hello World");
-        System.out.println("This is another line");
-        System.out.print("line after println");
-    }
-}
