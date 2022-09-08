d32b72c4283c89756472ce4ccaaa33be57d2be99
dh00mk3tu <cyberhybird@hotmail.com>
2022-09-08 11:06:52 +0530

    HalfDiamond

--- Module-1/Fundamentals/L6/HalfDiamond.java
+++ Module-1/Fundamentals/L6/HalfDiamond.java
@@ -14,12 +14,19 @@ public class HalfDiamond {
                 System.out.print(j);
                 // x--;
             }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
             System.out.print("*");
             System.out.println();
         }
-        for (int i = n-1; i >= 1; i--) {
+        for (int i = n - 1; i >= 1; i--) {
             System.out.print("*");
-            for (int j = i; j >= 1; j--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+                // x--;
+            }
+            for (int j = i - 1; j >= 1; j--) {
                 System.out.print(j);
             }
             System.out.print("*");
